e721ce2dce819b0d57af63861ff9a9223918cee1
sandraandeka <sandraandex13@gmail.com>
2022-03-11 15:58:36 -0800

    program that prints reserve

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - Entry point
+ *
+ * Return: Answer 0 (Success)
+ */
+int main(void)
+{
+	char alb;
+
+	for (alb = 'z'; alb >= 'a'; alb--)
+	{
+		putchar(alb);
+		putchar('\n');
+	}
+	return (0);
+}
