e71cf3a69bef11f73a502f710030d3d869144b9e
sandraandeka <sandraandex13@gmail.com>
2022-03-11 14:31:55 -0800

    program that writes alphabets both in uppercase and lowercase

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main -Entry point
+ *
+ * Return: Always 0 (Success)
+ *
+ */
+int main(void)
+{
+	char alb;
+
+	for (alb = 'a'; alb <= 'z'; alb++)
+	{
+		putchar(alb);
+	
+	for (alb = 'A'; alb <= 'Z'; alb++)
+	{
+		putchar(alb);
+	}
+	
+	putchar('\n');
+	
+	}
+	
+	return (0);
+}
+
+
