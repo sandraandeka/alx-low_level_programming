1499c186e4f1ddfc3811bd980224a2512bfc88f0
sandraandeka <sandraandex13@gmail.com>
2022-03-14 12:22:51 -0700

    print letters of base 16

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -4,22 +4,22 @@
 /**
  * main - Entry point
  *
- * Return :Always 0 (Success)
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
 	int d;
-        char low;
+	char l;
 
 	for (d = '0'; d <= '9'; d++)
 	{
 		putchar(d);
 	}
-	for (low = 'a'; low <= 'f'; low++)
+	for (l = 'a'; l <= 'f'; l++)
 	{
-		putchar(low);
-		putchar('\n');
+		putchar(l);
 	}
+		putchar('\n');
 
 	return (0);
 
