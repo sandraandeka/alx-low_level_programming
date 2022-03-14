d341939835282efa0991e40e4bb3fc3e9a09627d
sandraandeka <sandraandex13@gmail.com>
2022-03-14 13:58:41 -0700

    3 digits

---
+++ 0x01-variables_if_else_while/101-print_combo4.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+/**
+ * main - Entry Point
+ *
+ * Return: Answer 0 (success)
+ *
+ */
+int main(void)
+{
+	int i, j;
+
+	for (i = 0; i <= 99; i++)
+	{
+		for (j = 0; j <= 99; j++)
+		{
+			if (i < j && i != j)
+			{
+				putchar((i / 10) + '0');
+				putchar((i % 10) + '0');
+				putchar(' ');
+				putchar((j / 10) + '0');
+				putchar((j % 10) + '0');
+				putchar(' ');
+				if (i != 98 || j != 99)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
+
