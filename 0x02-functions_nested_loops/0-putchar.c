c2202836ceda37f0ce00f0f3a2b754efbbf98d2d
sandraandeka <sandraandex13@gmail.com>
2022-03-16 06:46:39 -0700

    program that print putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,23 +1,21 @@
-#include <unistd.h>
 #include "main.h"
 
 /**
  * main - Check description
- * Description: It prints the word _putchar, followed by a new line.
- * Return: 0. 
+ * description: It prints the word _putchar, followed by a new line.
+ * Return: Always 0 (success).
  *
  */
 int main(void)
 {
-	char word[8] = "_putchar";
-	int i;
-
-	for (i = 0; i < 8; i++)
-	{
-		_putchar(word[i]);
-	}
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('a');
+	_putchar('r');
 	_putchar('\n');
-
 
 	return (0);
 }
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,1 +1,11 @@
+#ifndef MAIN_H
+#define MAIN_H
 
+/**
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
+ *
+ * Return on success 1.
+ * On error, -1 is returned, and error is set appropriately.
+ */
+int _putchar(char c);
