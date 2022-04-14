4dfb8ed77e360362dc082091a6e67f6c37dd98a1
sandra andeka <85032976+sandraandeka@users.noreply.github.com>
2022-04-14 17:51:22 +0300

    Update 2-print_strings.c

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -22,7 +22,7 @@ void print_strings(const char *separator, const unsigned int n, ...)
 		if (str)
 			printf("%s", str);
 		else
-			printf("(nill)");
+			printf("(nil)");
 		if (i < n - 1 && separator)
 			printf("%s", separator);
 	}
